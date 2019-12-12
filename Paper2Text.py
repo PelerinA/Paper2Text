d780cd55431def03b74bc53b98cbdec6f2057de0
Anfosso <tommy.anfosso@univ-avignon.fr>
2019-12-12 11:27:34 +0100

    getTitle() + getAuteurs()

--- Paper2Text.py
+++ Paper2Text.py
@@ -13,12 +13,12 @@ class PaperEnt:
         self.biblio=biblio
 
     def toText(self):
-        return self.filename+'\n'+self.title+'\n'+self.abstract
+        return self.filename+'\n'+self.title+'\n'+self.auteur+self.abstract+'\n'+self.biblio
 
     def toXML(self):
         return """<article>\n
             <preamble>"""+self.filename+"""</preamble>\n
-            <title>"""+self.title+"""</titre>\n
+            <title>"""+self.title+"""</title>\n
             <auteur>"""+self.auteurs+"""</auteur>\n
             <abstract>"""+self.abstract+"""</abstract>\n
             <biblio>"""+self.biblio+"""</biblio>\n
@@ -38,10 +38,26 @@ class PersiFichierTexte:
 class Parser:
     def __init__(self,content):
         self.content=content
-    
-    # premiere ligne
+        dictionnaryPath = os.path.dirname(os.path.realpath(__file__))+os.path.sep+"firstnames.txt"
+        # firstnames est une liste de str
+        with open(dictionnaryPath, "r") as dictionnaryFile:
+            self.firstnames = dictionnaryFile.readlines()
+
+    # retourne le numero de la premiere ligne contenant un prenom
+    # on suppose que le fichier firstnames.txt se trouve dans le meme repertoire
+    def nbFirstLineWithName(self):
+        # parcourir le contenu a parser
+        fileLines = self.content.split("\n")
+        found = 0
+        for line in fileLines:
+            for firstname in self.firstnames:
+                if(line.find(firstname[:-1] + " ") != -1):
+                    return self.content.find(line)
+        
+
+    # premiere ligne ou contenu avant la premiere ligne contenant un prenom
     def getTitle(self):
-        return self.content.partition('\n')[0]
+        return self.content[0:self.nbFirstLineWithName()]
 
     # contenu entre abstract et introduction
     def getAbstract(self):
@@ -52,11 +68,11 @@ class Parser:
     
     # entre le titre et l'abstract
     def getAuteurs(self):
-        # ss = re.search('(?is)'+self.getTitle()+'(.*?)abstract',self.content)
-        # if ss:
-        #     return ss.group(1)
-        # return ""
-        pass
+        print(self.getTitle())
+        ss = re.search('(?is)'+self.getTitle()+'(.*?)abstract',self.content)
+        if ss:
+            return ss.group(1).replace('\n',' ')
+        return ""
     
     # derniere page ou après Aknowledgments et References
     def getBiblio(self):
@@ -86,17 +102,16 @@ def main():
             parser = Parser(PersiFichierTexte.persiToString(tmpDir+os.path.sep+filename))
             paper.filename=filename[:-4]+".pdf"
             paper.title=parser.getTitle()
+            paper.auteurs=parser.getAuteurs()
             paper.abstract=parser.getAbstract()
+
             # ecriture de l'entite Paper au format texte dans le dossier output
             # PersiFichierTexte.stringToPersi(paper.toText(),outputDir+os.path.sep+filename)
             PersiFichierTexte.stringToPersi(paper.toXML(),outputDir+os.path.sep+filename[:-3]+"xml")
 
+        # decommentez les 3 lignes du dessous pour debug
+        print(tmpDir)
+        while(1):
+            pass
+
 main()
-# paper = PaperEnt()
-# filename = "levner.txt"
-# filepath = os.path.dirname(os.path.realpath(__file__))+os.path.sep+filename
-# parser = Parser(PersiFichierTexte.persiToString(filepath))
-# paper.filename = filename
-# paper.title = parser.getTitle()
-# paper.abstract = parser.getAbstract()
-# print(paper.toText())
