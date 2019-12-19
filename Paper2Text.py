71c436f72ff36f31fb962d278024922033aade64
Benjamin Frisson <benjamin.frisson@alumni.univ-avignon.fr>
2019-12-19 11:46:46 +0100

    First commit

--- Paper2Text.py
+++ Paper2Text.py
@@ -5,24 +5,33 @@ import shutil
 import tempfile
 
 class PaperEnt:
-    def __init__(self,filename="",title="",abstract="",intro="",auteurs="",biblio=""):
+    def __init__(self,filename="",title="",abstract="",auteurs="",discussion="",biblio="", intro="", corps="", acknow="", conclusion=""):
         self.filename=filename
         self.title=title
         self.abstract=abstract
         self.intro=intro
         self.auteurs=auteurs
+        self.discussion=discussion
+        self.conclusion=conclusion
+        self.acknow=acknow
         self.biblio=biblio
+        self.corps=corps
 
     def toText(self):
-        return self.filename+'\n'+self.title+'\n'+self.auteurs+self.abstract+'\n'+self.intro+'\n'+self.biblio
+        return self.filename+'\n'+self.title+'\n'+self.auteurs+self.abstract+'\n'+self.intro+'\n'+self.corps+'\n'+self.discussion+'\n'+self.biblio
+
 
     def toXML(self):
         return """<article>\n
             <preamble>"""+self.filename+"""</preamble>\n
-            <titre>"""+self.title+"""</titre>\n
+            <title>"""+self.title+"""</title>\n
             <auteur>"""+self.auteurs+"""</auteur>\n
             <abstract>"""+self.abstract+"""</abstract>\n
             <introduction>"""+self.intro+"""</introduction>\n
+            <corps>"""+self.corps+"""</corps>\n
+            <discussion>"""+self.discussion+"""</discussion>\n
+            <conclusion>"""+self.conclusion+"""</conclusion>\n
+            <acknowledgments>"""+self.acknow+"""</acknowledgments>\n
             <biblio>"""+self.biblio+"""</biblio>\n
             </article>"""
 
@@ -71,16 +80,46 @@ class Parser:
     # entre le titre et l'abstract
     def getAuteurs(self):
         title = self.getTitle()
-        #print("Title: " + title)
         ss = re.search('(?is)'+title+'(.*?)abstract',self.content)
         if ss:
             return ss.group(1).replace('\n',' ')
         return ""
+
     # derniere page ou après Aknowledgments et References
     def getBiblio(self):
         ss = re.search('(?is)\nreferences\n(.*?)\Z',self.content)
         if ss:
             return ss.group(1).replace('\n',' ')
+        else:
+            ss = re.search('(?is)references(.*?)\Z',self.content)
+            if ss:
+                return ss.group(1).replace('\n',' ')
+        return ""
+
+    # avant la biblio
+    def getConclusion(self):
+        ss = re.search('(?is)conclusion(.*?)\Z',self.content)
+        if ss:
+            conclusion = ss.group(1)
+            conclusion =  '\n'.join(conclusion.split('\n')[1:]).replace('\n',' ')
+            tmp = self.getBiblio()
+            tmp2 = self.getAcknow()
+            return conclusion.replace(tmp,'').replace(tmp2,'')
+        return ""
+
+    def getAcknow(self):
+        ss = re.search('(?is)acknowledgments(.*?)\Z',self.content)
+        if ss:
+            acknow = ss.group(1).replace('\n',' ')
+            tmp = self.getBiblio()
+            return acknow.replace(tmp,'')
+        return ""
+
+    # contenu entre l'intro et la conclusion
+    def getCorps(self):
+        ss = re.search('(?is)\nintroduction.*?\n2(.*?)\nconclusion.*?\Z',self.content)
+        if ss:
+            return "2"+ss.group(1).replace('\n',' ')[:-2]
         return ""
 
     def getIntroduction(self):
@@ -89,7 +128,14 @@ class Parser:
             return ss.group(1).replace('\n',' ')
         return ""
 
-class Converter:
+    def getDiscussion(self):
+        ss = re.search('(?is)discussion\n(.*?)conclusion',self.content)
+        if ss:
+            return ss.group(1).replace('\n',' ')
+        return ""
+
+class Manager:
+
     def __init__(self):
         # on suppose que le targetDir est dans le meme repertoire que le script
         self.targetDir=os.path.dirname(os.path.realpath(__file__))+os.path.sep+sys.argv[1]
@@ -104,14 +150,20 @@ class Converter:
         os.mkdir(self.outputDir, 0o755)
         print("Répertoire output crée")
         os.mkdir(self.tmpDir, 0o755)
-        print("Répertoire tmp crée")
+        self.analyseTargetFolder()
+        index = 0
+        self.choices = []
+        for file in self.files:
+            self.choices.insert(index, 0)
 
     def createTemporaryFiles(self):
         # convertir les PDF en format txt avec pdftotext dans un dossier tmp
-            for filename in os.listdir(self.targetDir):
-                if filename.endswith('.pdf'):
+            index = 0
+            for filename in self.files:
+                if self.choices[index] == 1 and filename.endswith('.pdf'):
                     f = filename.replace(" ","\ ")
                     os.system("pdftotext "+self.targetDir+os.path.sep+f+" "+self.tmpDir+os.path.sep+f[:-3]+"txt")
+                index += 1
 
     def convert(self):
         # # deposer les sorties au format .txt, avec meme nom que pdf respectif
@@ -124,7 +176,9 @@ class Converter:
             paper.auteurs=parser.getAuteurs()
             paper.abstract=parser.getAbstract()
             paper.intro=parser.getIntroduction()
+            paper.discussion=parser.getDiscussion()
             paper.biblio=parser.getBiblio()
+            paper.corps=parser.getCorps()
             # ecriture de l'entite Paper au format texte dans le dossier output
             if len(sys.argv) <= 2 or sys.argv[2] == "-t" :
                 PersiFichierTexte.stringToPersi(paper.toText(),self.outputDir+os.path.sep+filename)
@@ -137,9 +191,55 @@ class Converter:
     def removeTemporaryFolder(self):
         if os.path.exists(self.tmpDir):
                 shutil.rmtree(self.tmpDir)
-                print("Répertoire tmp supprimé")
+
+    def displayListOfFiles(self):
+        print("Liste des fichiers pris en charges :")
+        index = 0
+        for file in self.files:
+            chosen = " "
+            if(self.choices[index] == 1):
+                chosen = "X"
+            print(str(index) + ". " + chosen + " " + file)
+            index += 1
+
+    def askChoiceInput(self):
+        self.displayListOfFiles()
+        print("---------------------")
+        choice = input("Veuillez choisir un fichier à convertir par son numéro (entrez 'c' pour lancer la conversion, 'T' pour une jolie bannière): ")
+        if choice.isdigit():
+            if int(choice)+1 > len(self.files):
+                print("ERREUR: Aucun fichier n'est identifié par l'entrée donnée")
+            else:
+                if (self.choices[int(choice)] == 1):
+                    self.choices[int(choice)] = 0
+                else:
+                    self.choices[int(choice)] = 1
         else:
-            print("Le repertoire tmp ne peut être supprimé, il n'existe pas")
+            if(choice == "c"):
+                return 1
+            elif(choice == "T"):
+                self.displayBanner()
+            else:
+                print("ERREUR: Commande inconnue")
+        return 0
+
+    def choiceLoop(self):
+        loopOver = 0
+        while loopOver != 1:
+            loopOver = self.askChoiceInput()
+    
+    def analyseTargetFolder(self):
+        index = 0
+        self.files = []
+        for filename in os.listdir(self.targetDir):
+            if filename.endswith(".pdf"):
+                self.files.insert(index, filename)
+                index += 1
+
+    def displayBanner(self):
+        print("")
+        print(PersiFichierTexte.persiToString("art"))
+        print("")
 
     def removeOutputFolder(self):
         if os.path.exists(self.outputDir):
@@ -149,11 +249,12 @@ class Converter:
             print("Le repertoire output ne peut être supprimé, il n'existe pas")
 
 
-
 def main():
-    converter = Converter()
-    converter.createTemporaryFiles()
-    converter.convert()
-    converter.removeTemporaryFolder()
+    manager = Manager()
+    manager.displayBanner()
+    manager.choiceLoop()
+    manager.createTemporaryFiles()
+    manager.convert()
+    manager.removeTemporaryFolder()
 
 main()
