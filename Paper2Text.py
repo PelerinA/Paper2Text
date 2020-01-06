1a89303d976c06c0992addc2e61ebea824771c2c
AlexMilos <57668315+AlexMilos@users.noreply.github.com>
2020-01-06 18:50:25 +0100

    Final

--- Paper2Text.py
+++ Paper2Text.py
@@ -1,3 +1,28 @@
+###################################################################################################
+##                                                                                               ##
+##                                        Copyright (C) 2020                                     ##
+##          Pelerin Aurelien, Frisson Ben, Anfosso Tommy, Monteil Alison, Jacob Alex             ##
+##                                                                                               ##
+##                                    Licensed under GPLv3                                       ##
+##                          https://www.gnu.org/licenses/gpl-3.0.html                            ##
+##                                                                                               ##
+##          This program is free software: you can redistribute it and/or modify                 ##
+##          it under the terms of the GNU General Public License as published by                 ##
+##          the Free Software Foundation, either version 3 of the License, or                    ##
+##          (at your option) any later version.                                                  ##
+##                                                                                               ##
+##          This program is distributed in the hope that it will be useful,                      ##
+##          but WITHOUT ANY WARRANTY; without even the implied warranty of                       ##
+##          MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                        ##
+##          GNU General Public License for more details.                                         ##
+##                                                                                               ##
+##          You should have received a copy of the GNU General Public License                    ##
+##          along with this program.  If not, see <https://www.gnu.org/licenses/>.               ##
+##                                                                                               ##
+##                                                                                               ##
+###################################################################################################
+
+
 import sys
 import os
 import re
@@ -326,7 +351,7 @@ class Manager:
 
 def main():
     manager = Manager()
-    #manager.displayBanner()
+    manager.displayBanner()
     manager.choiceLoop()
     manager.createTemporaryFiles()
     manager.convert()
